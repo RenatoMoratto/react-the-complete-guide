6215fd9454ec02985c92faa71defa27995fa5ce2
Facesoccer <80411445+RenatoMoratto@users.noreply.github.com>
2021-09-06 13:42:27 -0300

    refactor: implement all ctx config in one file
    Lesson: 124

--- 05-side-effects-use-effect-context-api/src/components/Login/Login.js
+++ 05-side-effects-use-effect-context-api/src/components/Login/Login.js
@@ -1,8 +1,14 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -42,6 +48,8 @@ const Login = (props) => {
     value: '',
     isValid: null,
   });
+
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     console.log('Effect Running!!');
@@ -91,7 +99,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
