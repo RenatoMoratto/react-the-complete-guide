d436db92557fe449c76a878f9259ef774b40f55f
RenatoMoratto <renatowmoratto@gmail.com>
2021-12-14 12:45:34 -0300

    feat: Aggregate Item Total Amount
    Shows only one item of each type in cart with the total Amount aggregated

--- 06-food-order-app/src/store/CartProvider.js
+++ 06-food-order-app/src/store/CartProvider.js
@@ -9,9 +9,28 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.totalAmount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        totalAmount: existingCartItem.totalAmount + action.item.totalAmount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
