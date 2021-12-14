a2b92fd2eff253ae5bafb7245c0b28a506874fd2
RenatoMoratto <renatowmoratto@gmail.com>
2021-12-14 13:22:04 -0300

    feat: visual bump on each add item to cart

--- 06-food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ 06-food-order-app/src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../../store/cart-context';
 import CartIcon from '../../Cart/CartIcon/CartIcon.js';
@@ -6,14 +6,32 @@ import CartIcon from '../../Cart/CartIcon/CartIcon.js';
 import style from './HeaderCartButton.module.css';
 
 function HeaderCartButton(props) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnStyles = `${style.button} ${btnIsHighlighted && style.bump}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={style.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
       <span className={style.icon}>
         <CartIcon />
       </span>
